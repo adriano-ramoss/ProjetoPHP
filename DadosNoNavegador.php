84dbc91e68057850952f32bbdba4f17e8604f2f1
adriano-ramoss <adrianoramos2401@gmail.com>
2023-03-01 19:51:25 -0300

    DadosNoNavegador

---
+++ DadosNoNavegador.php
@@ -0,0 +1,12 @@
+<?php 
+print "Usando print para exibir na tela <br/>";
+
+$data = "20 de agosto de 2020";
+$salario = 850.00;
+$cargo = "Estagiario";
+
+echo "Arquivo criado em $data <br/>";
+printf("Salàrio mínimo: R$ %.2f por mês  <br/>", $salario); //%.2f adiciona o valor de salario no print
+$texto = sprintf("%s recebe R$ %.2f por mês", $cargo, $salario*2); //%s adiciona o valor da string
+echo $texto;
+?>
