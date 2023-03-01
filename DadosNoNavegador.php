c5b497f7e52dce3f54c437791030ca3f21674680
adriano-ramoss <adrianoramos2401@gmail.com>
2023-03-01 20:20:09 -0300

    DadosNoNavegador

--- DadosNoNavegador.php
+++ DadosNoNavegador.php
@@ -4,7 +4,6 @@ print "Usando print para exibir na tela <br/>";
 $data = "20 de agosto de 2020";
 $salario = 850.00;
 $cargo = "Estagiario";
-$teste = "AAAAA";
 
 echo "Arquivo criado em $data <br/>";
 printf("Salàrio mínimo: R$ %.2f por mês  <br/>", $salario); //%.2f - adiciona o valor de salario no print
---
+++ calculadora.php
@@ -0,0 +1,14 @@
+<?php
+/*$tabuada = 5;
+printf("O resultado é ");
+echo $tabuada;
+*/
+ $n1 = 5;
+ $n2 = 5;
+ $res = $n1 + $n2;
+ printf("%s + %s = ", $n1, $n2);
+ echo $res;
+
+?>
+
+
