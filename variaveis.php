5dc3a75cd2f0259c8002f4c5521d46a2b1ead111
adriano-ramoss <adrianoramos2401@gmail.com>
2023-03-01 19:31:57 -0300

    variavel

---
+++ variaveis.php
@@ -0,0 +1,14 @@
+<?php  
+
+$data =  "Data De Agosto De 2020"; //String
+$salario = 850.00; //Double
+$cargo = "Aprendiz";
+$idade = 18; //int
+$resultado = true; //boolean
+if ($resultado){
+
+    echo "Verdadeiro";
+}
+
+
+?>
