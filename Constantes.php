ac925c26f9c46ab4b9d800dbbf018a9d6ca83a49
adriano-ramoss <adrianoramos2401@gmail.com>
2023-03-08 19:51:30 -0300

    Adição do arquivos Constantes

---
+++ Constantes.php
@@ -0,0 +1,18 @@
+<?php 
+/*
+$count = 0;
+$soma = 2;
+
+while($count <= 1000){
+    print( $soma ." + ". $count ." = " .($soma+$count)."<br>");
+$count++;
+}
+*/
+
+define ("PI", 3.14);
+define ("DISC", "MATEMÁTICA");
+
+echo "<br/> Valor de PI: ". PI;
+echo "<br/> Disciplina: " .DISC;
+
+?>
